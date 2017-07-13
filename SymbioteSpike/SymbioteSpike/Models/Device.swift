a2545fcd0cffe462641b4bf0384e66674420674d
Konrad Leszczyński <konrri@man.poznan.pl>
2017-07-13 11:22:42 +0200

    Device model template

---
+++ SymbioteSpike/SymbioteSpike/Models/Device.swift
@@ -0,0 +1,18 @@
+//
+//  Device.swift
+//  SymbioteSpike
+//
+//  Created by Konrad Leszczyński on 13/07/2017.
+//  Copyright © 2017 Konrad Leszczyński. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Device {
+    convenience init(j: JSON)  {
+        self.init()
+        
+    }
+    
+}
