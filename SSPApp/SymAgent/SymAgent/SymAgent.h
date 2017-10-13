b983f113a5c578681e71573fcffea3125b8e02dd
Konrad Leszczyński <konrri@man.poznan.pl>
2017-10-13 16:25:29 +0200

    symAgent- step2

---
+++ SSPApp/SymAgent/SymAgent/SymAgent.h
@@ -0,0 +1,19 @@
+//
+//  SymAgent.h
+//  SymAgent
+//
+//  Created by Konrad Leszczyński on 13/10/2017.
+//  Copyright © 2017 PSNC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SymAgent.
+FOUNDATION_EXPORT double SymAgentVersionNumber;
+
+//! Project version string for SymAgent.
+FOUNDATION_EXPORT const unsigned char SymAgentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SymAgent/PublicHeader.h>
+
+
