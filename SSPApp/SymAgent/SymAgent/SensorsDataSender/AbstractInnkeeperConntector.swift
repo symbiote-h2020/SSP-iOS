1a1739cf4c3a6e663d67c40eba07ff2675f05c2c
Konrad Leszczyński <konrri@man.poznan.pl>
2017-12-15 15:57:46 +0100

    innkeeper connector

---
+++ SSPApp/SymAgent/SymAgent/SensorsDataSender/AbstractInnkeeperConntector.swift
@@ -0,0 +1,16 @@
+//
+//  AbstractInnkeeperConntector.swift
+//  SymAgent
+//
+//  Created by Konrad Leszczyński on 15/12/2017.
+//  Copyright © 2017 PSNC. All rights reserved.
+//
+
+import Foundation
+
+
+//methods to implement  https://colab.intracom-telecom.com/display/SYM/Interface:+messages+sent+to+SSP
+
+class AbstractInnkeeperConntector {
+    
+}
