bd088af2bb7d79b5ccdb3c6752031209244ef348
Konrad Leszczyński <konrri@man.poznan.pl>
2018-07-06 15:03:56 +0200

    security common library

---
+++ SSPApp/SecuritySSP/SecuritySSP/CertificateSigningRequestSwift/CertificateSigningRequestSwiftTests/CertificateSigningRequestSwiftTests.swift
@@ -0,0 +1,351 @@
+//
+//  CertificateSigningRequestSwiftTests.swift
+//  CertificateSigningRequestSwiftTests
+//
+//  Created by Corey Baker on 11/7/16.
+//  Copyright © 2016 One Degree Technologies. All rights reserved.
+//
+
+import XCTest
+import Foundation
+//@testable import CertificateSigningRequestSwift
+import CertificateSigningRequestSwift //Only testing public functions
+
+//NOTE: Testcases won't work when testing within framework. I believe this because an Application needs to have an entitlement to have Keychain access
+
+class CertificateSigningRequestSwiftTests: XCTestCase {
+    
+    var publicKey: SecKey?
+    var privateKey: SecKey?
+    var keyBlockSize: Int?
+    var publicKeyBits: Data?
+    let keyAlgoorithm = KeyAlgorithm.ec(signatureType: .sha256)
+    
+    let tagPublic = "com.csr.public"
+    let tagPrivate = "com.csr.private"
+    let keyAlgorithm = KeyAlgorithm.ec(signatureType: .sha256)
+    
+    override func setUp() {
+        super.setUp()
+        
+        if (self.publicKey == nil) && (self.privateKey == nil) && self.keyBlockSize == nil {
+            
+            let publicKeyParameters: [String: AnyObject] = [
+                String(kSecAttrIsPermanent): kCFBooleanTrue,
+                String(kSecAttrApplicationTag): tagPublic as AnyObject,
+                String(kSecAttrAccessible): kSecAttrAccessibleAlways
+            ]
+            
+            var privateKeyParameters: [String: AnyObject] = [
+                String(kSecAttrIsPermanent): kCFBooleanTrue,
+                String(kSecAttrApplicationTag): tagPrivate as AnyObject,
+                String(kSecAttrAccessible): kSecAttrAccessibleAlways
+            ]
+            
+            #if !arch(i386) && !arch(x86_64)
+                //This only works for Secure Enclave consistign of 256 bit key, note, the signatureType is irrelavent for this check
+                if keyAlgorithm.type == KeyAlgorithm.ec(signatureType: .sha1).type{
+                    let access = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
+                                                                 kSecAttrAccessibleAlwaysThisDeviceOnly,
+                                                                 .privateKeyUsage,
+                                                                 nil)!   // Ignore error
+                    
+                    privateKeyParameters[String(kSecAttrAccessControl)] = access
+                }
+            #endif
+            
+            //Define what type of keys to be generated here
+            var parameters: [String: AnyObject] = [
+                String(kSecAttrKeyType): keyAlgorithm.secKeyAttrType,
+                String(kSecAttrKeySizeInBits): keyAlgorithm.availableKeySizes.last! as AnyObject,
+                String(kSecReturnRef): kCFBooleanTrue,
+                kSecPublicKeyAttrs as String: publicKeyParameters as AnyObject,
+                kSecPrivateKeyAttrs as String: privateKeyParameters as AnyObject,
+                ]
+            
+            #if !arch(i386) && !arch(x86_64)
+                
+                //iOS only allows EC 256 keys to be secured in enclave. This will attempt to allow any EC key in the enclave, assuming iOS will do it outside of the enclave if it doesn't like the key size, note: the signatureType is irrelavent for this check
+                if keyAlgorithm.type == KeyAlgorithm.ec(signatureType: .sha1).type{
+                    parameters[String(kSecAttrTokenID)] = kSecAttrTokenIDSecureEnclave
+                }
+                
+            #endif
+            
+            //Use Apple Security Framework to generate keys, save them to application keychain
+            var error: Unmanaged<CFError>?
+            self.privateKey = SecKeyCreateRandomKey(parameters as CFDictionary, &error)
+            
+            if self.privateKey == nil{
+                print("Error creating keys occured: \(error!.takeRetainedValue() as Error), keys weren't created")
+                return
+            }
+            
+            //Get generated public key
+            let query: [String: AnyObject] = [
+                String(kSecClass): kSecClassKey,
+                String(kSecAttrKeyType): keyAlgorithm.secKeyAttrType,
+                String(kSecAttrApplicationTag): tagPublic as AnyObject,
+                String(kSecReturnRef): kCFBooleanTrue
+            ]
+            
+            var publicKeyReturn:AnyObject?
+            
+            let result = SecItemCopyMatching(query as CFDictionary, &publicKeyReturn)
+            
+            if result != errSecSuccess{
+                print("Error getting publicKey fron keychain occured: \(result)")
+                return
+            }
+            
+            self.publicKey = publicKeyReturn as! SecKey?
+        }
+        
+        //Set block size
+        self.keyBlockSize = SecKeyGetBlockSize(self.publicKey!)
+        
+        //Ask keychain to provide the publicKey in bits
+        let query: [String: AnyObject] = [
+            String(kSecClass): kSecClassKey,
+            String(kSecAttrKeyType): keyAlgorithm.secKeyAttrType,
+            String(kSecAttrApplicationTag): tagPublic as AnyObject,
+            String(kSecReturnData): kCFBooleanTrue
+        ]
+        
+        var tempPublicKeyBits:AnyObject?
+        
+        _ = SecItemCopyMatching(query as CFDictionary, &tempPublicKeyBits)
+        
+        guard let keyBits = tempPublicKeyBits as? Data else {
+            return
+        }
+        
+        publicKeyBits = keyBits
+        
+        //Initiale CSR
+        let csr = CertificateSigningRequest(commonName: "CertificateSigningRequestSwift Test", organizationName: "Test", organizationUnitName: "Test", countryName: "US", stateOrProvinceName: "KY", localityName: "Test", keyAlgorithm: keyAlgorithm)
+        //Build the CSR
+        guard let csrBuild = csr.buildAndEncodeDataAsString(publicKeyBits!, privateKey: privateKey!) else {
+            return
+        }
+        
+        print("CSR string no header and footer")
+        print(csrBuild)
+        
+        guard let csrBuild2 = csr.buildCSRAndReturnString(publicKeyBits!, privateKey: privateKey!) else {
+            return
+        }
+        
+        print("CSR string with header and footer")
+        print(csrBuild2)
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testiOSKeyCreation(_ test:Bool=true) {
+        // This is an example of a functional test case.
+        if (publicKey != nil) && (privateKey != nil) && keyBlockSize != nil{
+            //Keys only need to be created once, after they can be used over again
+            return
+        }
+        
+        let tagPublic = "com.testing.ioscsrswift.public"
+        let tagPrivate = "com.testing.ioscsrswift.private"
+        
+        let publicKeyParameters: [String: AnyObject] = [
+            String(kSecAttrIsPermanent): kCFBooleanTrue,
+            String(kSecAttrApplicationTag): tagPublic as AnyObject,
+            String(kSecAttrAccessible): kSecAttrAccessibleAlways
+        ]
+        
+        var privateKeyParameters: [String: AnyObject] = [
+            String(kSecAttrIsPermanent): kCFBooleanTrue,
+            String(kSecAttrApplicationTag): tagPrivate as AnyObject,
+            String(kSecAttrAccessible): kSecAttrAccessibleAlways
+        ]
+        
+        #if !arch(i386) && !arch(x86_64)
+            //This only works for Secure Enclave consistign of 256 bit key, note, the signatureType is irrelavent for this check
+            if keyAlgorithm.type == KeyAlgorithm.ec(signatureType: .sha1).type{
+                let access = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
+                                                             kSecAttrAccessibleAlwaysThisDeviceOnly,
+                                                             .privateKeyUsage,
+                                                             nil)!   // Ignore error
+                
+                privateKeyParameters[String(kSecAttrAccessControl)] = access
+            }
+        #endif
+        
+        //Define what type of keys to be generated here
+        var parameters: [String: AnyObject] = [
+            String(kSecReturnRef): kCFBooleanTrue,
+            kSecPublicKeyAttrs as String: publicKeyParameters as AnyObject,
+            kSecPrivateKeyAttrs as String: privateKeyParameters as AnyObject,
+            ]
+        
+        parameters[String(kSecAttrKeySizeInBits)] = keyAlgoorithm.availableKeySizes.last! as AnyObject
+        
+        if #available(iOS 10, *) {
+            parameters[String(kSecAttrKeyType)] = keyAlgoorithm.secKeyAttrType
+        } else {
+            // Fallback on earlier versions
+            parameters[String(kSecAttrKeyType)] = keyAlgoorithm.secKeyAttrTypeiOS9
+        }
+        
+        #if !arch(i386) && !arch(x86_64)
+            
+            //iOS only allows EC 256 keys to be secured in enclave. This will attempt to allow any EC key in the enclave, assuming iOS will do it outside of the enclave if it doesn't like the key size, note: the signatureType is irrelavent for this check
+            if keyAlgorithm.type == KeyAlgorithm.ec(signatureType: .sha1).type{
+                parameters[String(kSecAttrTokenID)] = kSecAttrTokenIDSecureEnclave
+            }
+            
+        #endif
+        
+        //Use Apple Security Framework to generate keys, save them to application keychain
+        if #available(iOS 10.0, *) {
+            
+            var error: Unmanaged<CFError>?
+            self.privateKey = SecKeyCreateRandomKey(parameters as CFDictionary, &error)
+            
+            if self.privateKey == nil{
+                XCTAssert(false, "Error occured: \(error!.takeRetainedValue() as Error), keys weren't created")
+                return
+            }
+            
+            //Get generated public key
+            let query: [String: AnyObject] = [
+                String(kSecClass): kSecClassKey,
+                String(kSecAttrKeyType): keyAlgoorithm.secKeyAttrType,
+                String(kSecAttrApplicationTag): tagPublic as AnyObject,
+                String(kSecReturnRef): kCFBooleanTrue
+            ]
+            
+            var publicKeyReturn:AnyObject?
+            
+            let result = SecItemCopyMatching(query as CFDictionary, &publicKeyReturn)
+            
+            if result != errSecSuccess{
+                XCTAssert(false,"Error occured: \(result)")
+                return
+            }
+            
+            self.publicKey = publicKeyReturn as! SecKey?
+            
+        } else {
+            // Fallback on earlier versions
+            
+            let result = SecKeyGeneratePair(parameters as CFDictionary, &publicKey, &privateKey)
+            
+            if result != errSecSuccess{
+                XCTAssert(false, "Error occured: \(result), keys weren't created")
+                return
+            }
+        }
+        
+        print("Public and private key pair created")
+        
+        guard publicKey != nil else {
+            if test{
+                XCTAssert(false, "Error  in setUp(). PublicKey shouldn't be nil")
+            }
+            return
+        }
+        
+        guard privateKey != nil else{
+            if test{
+                XCTAssert(false, "Error  in setUp(). PrivateKey shouldn't be nil")
+            }
+            return
+        }
+        
+        //Set block size
+        keyBlockSize = SecKeyGetBlockSize(publicKey!)
+        
+        //Ask keychain to provide the publicKey in bits
+        var query: [String: AnyObject] = [
+            String(kSecClass): kSecClassKey,
+            String(kSecAttrApplicationTag): tagPublic as AnyObject,
+            String(kSecReturnData): kCFBooleanTrue
+        ]
+        
+        if #available(iOS 10, *) {
+            query[String(kSecAttrKeyType)] = self.keyAlgoorithm.secKeyAttrType
+        } else {
+            // Fallback on earlier versions
+            query[String(kSecAttrKeyType)] = self.keyAlgoorithm.secKeyAttrTypeiOS9
+        }
+        
+        var tempPublicKeyBits:AnyObject?
+        
+        let result = SecItemCopyMatching(query as CFDictionary, &tempPublicKeyBits)
+        
+        switch result {
+        case errSecSuccess:
+            
+            guard let keyBits = tempPublicKeyBits as? Data else {
+                if test{
+                    XCTAssert(false, "Error: couldn't cast publicKeyBits from AnyObject to Data")
+                }
+                return
+            }
+            
+            publicKeyBits = keyBits
+            
+            if test{
+                XCTAssert(true, "Pass")
+            }
+            
+        default:
+            if test{
+                XCTAssert(false, "Error when retrieving publicKey in bits from the keychain: \(result)")
+            }
+        }
+    }
+    
+    func testCSRStandardInitializer() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //Ensure keys are available
+        self.testiOSKeyCreation(false)
+        
+        if (publicKey != nil) && (privateKey != nil) && keyBlockSize != nil{
+            //Keys only need to be created once, after they can be used over again
+            XCTAssert(false, "Keys were not created")
+            return
+        }
+        
+        let csr = CertificateSigningRequest()
+        
+        guard let csrBuild = csr.build(publicKeyBits!, privateKey: privateKey!) else{
+            XCTAssert(false, "Could not build CSR")
+            return
+        }
+        
+        guard let csrString = csrBuild.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0)).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) else{
+            XCTAssert(false, "Could not encode CSR to string")
+            return
+        }
+        
+        if !csrString.isEmpty{
+            XCTAssert(true, csrString)
+        }else{
+            
+            XCTAssert(false, "Encoded CSR string was empty")
+        }
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ SSPApp/SecuritySSP/SecuritySSP/CertificateSigningRequestSwift/README.md
@@ -0,0 +1,37 @@
+# CertificateSigningRequestSwift
+![Swift Version 4](https://img.shields.io/badge/Swift-v4-yellow.svg)
+
+Generate a certificate signing request (CSR) in iOS using Swift
+
+**NEW: A simple mobile application is available to test this framework here: https://github.com/cbaker6/CertificateSigningRequestSwift_Test**
+
+This is a port of ios-csr by Ales Teska (https://github.com/ateska/ios-csr) from Objective-C to Swift 4.0 (a Swift 3.2 version is available on the "3.2" branch).
+Additions have been made to support RSA and EC (iOS only supports 256 bit keys for now) allow SHA256 and SHA512. Also, this is setup to be added as a framework to your project.
+
+To use, initiatlize the class using one of the following (an example of how to do can be found at https://github.com/cbaker6/CertificateSigningRequestSwift/blob/master/CertificateSigningRequestSwiftTests/CertificateSigningRequestSwiftTests.swift): 
+- `let csr = CertificateSigningRequest()`
+- `let csr = CertificateSigningRequest(keyAlgorithm: KeyAlgorithm)`
+- `let csr = CertificateSigningRequest(commonName: String?, organizationName:String?, organizationUnitName:String?, countryName:String?, stateOrProvinceName:String?, localityName:String?, keyAlgorithm: KeyAlgorithm)`
+
+Then simply build your CSR using your publicKey(bits) and privateKey using, `let builtCSR = csr.buildCSRAndReturnString(publicKeyBits, privateKey: privateKey)`.
+
+Two other methods are available depending on your needs.
+
+- To get CSR without header and footer info use: `let builtCSR = csr.buildAndEncodeDataAsString(publicKeyBits, privateKey: privateKey)`.
+- To get CSR as Data use: `let builtCSR = csr.build(publicKeyBits, privateKey: privateKey)`.
+
+Note1: To use out of the box, build the project, look under frameworks, and drag "CertificateSigningRequestSwift.framework" into your project. You will need to do this in two places:
+
+- In your project Targets, click on "General"
+- Place "CertificateSigningRequestSwift.framework" in "Embedded Binaries" and it should automatically appear in "Linked Frameworks and Libraries"
+- Then, simply place "import CertificateSigningRequestSwift" at the top of any file that needs the framework.
+
+Note2: You can get your publicKey in bit by querying it from the iOS keychain using `String(kSecReturnData): kCFBooleanTrue` in your query (see "testiOSKeyCreation()" in CertificateSigningRequestSwiftTests.swift).  An app to test the framework is available here: https://github.com/cbaker6/CertificateSigningRequestSwift_Test. Just run the test and the CSR will be printing in the debug window. You can test if the CSR was created correctly here: https://redkestrel.co.uk/products/decoder/
+
+Note3: Do not try to run the testcase from within the framework, it **WILL FAIL**. I believe this is because a framework doesn't have the same entitlements as an application and therefore doesn't have access to a keychain. You should be able to run the testcase by copy/pasting it inside of your own application unit test.
+
+~~Note4: If you use this as a framework, you will need to go to Targets->APPNAME->General and add "CertificateSigningRequestSwift.framework" to Embedded Binaries". You may need to add "CommonCrypto.framework" as well. If you just use want to use CertificateSigningRequest.swift you will need to import CommonCrypto into your project. To do this follow the directions here: http://stackoverflow.com/questions/25248598/importing-commoncrypto-in-a-swift-framework?answertab=votes#tab-top~~
+
+**If anyone would like to help prepare this project for cocoapods, I could use your help. Please see the "testingCocoaPods" branch. I currently can't get the project to pass lint.**
+
+Feel free to use in your projects and contribute to this one.
