b17b8962e49f6a802201575813d4682c249595d5
Konrad Leszczyński <konrri@man.poznan.pl>
2018-07-17 12:02:34 +0200

    projects hierarhy cleaning

---
+++ SSPApp/SecuritySSP/SecuritySSP/LightweightSecurityProtocol/LwspManager.swift
@@ -0,0 +1,13 @@
+//
+//  LwspManager.swift
+//  SecuritySSP
+//
+//  Created by Konrad Leszczyński on 01/06/2018.
+//  Copyright © 2018 Konrad. All rights reserved.
+//
+
+import Foundation
+
+class LwspManager {
+    
+}
