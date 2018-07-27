26c34388db1da69a8dbeb513f1bc8b3b7e9fea81
Konrad Leszczyński <konrri@man.poznan.pl>
2018-07-27 10:57:08 +0200

    packageg manager

---
+++ SSPApp/SecuritySSP/.build/x86_64-apple-macosx10.10/debug/CNIOOpenSSL.build/empty.c.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/konrri/Work/git_stash/Symbiote/SSP-iOS/SSPApp/SecuritySSP/.build/checkouts/swift-nio-ssl-support.git--2359138821295600615/module.modulemap \
+  /Users/konrri/Work/git_stash/Symbiote/SSP-iOS/SSPApp/SecuritySSP/.build/checkouts/swift-nio-ssl.git-1370587408992578247/Sources/CNIOOpenSSL/empty.c
